80410d42addd6a81f0ce7e84081dd1ae53a7bae5
Frogluba <kisielytekarolina@gmail.com>
2024-03-12 20:25:26 +0200

    Needs lots of changes

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class Fruit : MonoBehaviour
 {
-    public GameObject colidded;
+    public GameObject colided;
     public GameObject apeared;
+    private bool hasCollided = false;
 
     private void Update()
     {
-        
+
     }
 
-    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!hasCollided && collision.gameObject.CompareTag(this.gameObject.tag))
+        {
+            
+
+            // Destroy both colliding objects
+            Destroy(this.gameObject);
+            Destroy(collision.gameObject);
+
+            // Instantiate a new object at the collision point
+            Instantiate(apeared, collision.GetContact(0).point, Quaternion.identity);
+        }
+    }
+
 
 }
