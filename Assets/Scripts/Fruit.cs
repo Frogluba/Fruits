e8d6378d395a4548a345e1566c69ba176526f9f7
Frogluba <146203655+Frogluba@users.noreply.github.com>
2024-03-13 19:58:48 +0200

    done?

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -1,22 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Reflection;
-using UnityEditorInternal;
 using UnityEngine;
+using TMPro;
 
 public class Fruit : MonoBehaviour
 {
+    public int points = 1;
+    public bool isMerging;
+    public GameObject nextFruit;
+    public AudioClip sound;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private  void OnCollisionEnter2D(Collision2D collision)
     {
-        if (!collision.gameObject.CompareTag("Fruit")) return;
-        
+        if (isMerging) return; 
+       if(collision.gameObject.CompareTag("Cherry"))
+        {
+            var source = GetComponent<AudioSource>();
+            source.clip = sound;
+            source.Play();
+        }
+        if (!collision.gameObject.CompareTag("Strawberry")) return;
+        if (points != collision.gameObject.GetComponent<Fruit>().points) return;
+
+        isMerging = true;
+        collision.gameObject.GetComponent<Fruit>().isMerging = true;
+
         var middlePos = (transform.position + collision.transform.position)/ 2;
 
         Destroy(gameObject);
         Destroy(collision.gameObject);
 
-        print("fruit");
+        Instantiate(nextFruit, middlePos, Quaternion.identity);
+        
     }
+    
 
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,34 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Spawner : MonoBehaviour
 {
     public List<GameObject> fruits;
-    public GameObject text1;
-    public GameObject text2;
-
-
-    void Update()
+    public TMP_Text  text1;
+    public TMP_Text text2;
+  
+    void  Update()
     {
         Vector2 mousePosition = Input.mousePosition;
         Vector2 worldMousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
         float clampedX = Mathf.Clamp(worldMousePosition.x, Camera.main.ViewportToWorldPoint(Vector3.zero).x, Camera.main.ViewportToWorldPoint(Vector3.one).x);
         transform.position = new Vector2(clampedX, transform.position.y);
         OnClick();
+        
     }
 
-    public void OnClick()
+     public void OnClick()
     {
-        
-
+  
         if (Input.GetMouseButtonDown(0))
         {
-            text1.active = false;
-            text2.active = false;
-            print("fruit");
+            text1.text = "";
+            text2.text= "";
+            
             int randomIndex = Random.Range(0, fruits.Count);
             GameObject selectedObject = Instantiate(fruits[randomIndex], transform.position, Quaternion.identity);
         }
+
+        
     }
 }
